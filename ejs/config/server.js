969ce81de702a57e4be59f03236068646bc5f989
Gabriel de Freitas <gabrieldfreitas9@gmail.com>
2018-03-06 21:18:12 -0300

    criado método post

--- ejs/config/server.js
+++ ejs/config/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const consign = require('consign');
+const bodyParser = require('body-parser')
 const constants = require('../../common/constants');
 
 const server = express();
@@ -7,7 +8,13 @@ const server = express();
 server.set('view engine', 'ejs');
 server.set('views', './views');
 
-server.listen(constants.SERVER_PORT, 
+// parse application/x-www-form-urlencoded
+server.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+server.use(bodyParser.json())
+
+server.listen(constants.SERVER_PORT,
    () => console.log(`Server is running on port ${constants.SERVER_PORT}`)
 )
 
--- ejs/routes/home.js
+++ ejs/routes/home.js
@@ -1,5 +1,14 @@
 const template = require('../../common/template-builder')
 
-module.exports = server => server.get('/', (req, res) => {
-   res.send(template.build('<h1>Homepage</h1>'))
-})
+module.exports = server => {
+   server.get('/', (req, res) => {
+      res.send(template.build('<h1>Homepage</h1>'))
+   })
+
+   server.post('/', (req, res) => {
+      res.send(JSON.stringify({
+         state: "received",
+         content: req.body
+      }));
+   })
+}
