652b18758bf975eaaaa32c1122c1622711e42d1c
Gabriel De Freitas <gabrieldfreitas9@gmail.com>
2018-11-18 18:34:22 -0200

    atualizacao

--- standard-modules/crypto/crypto.js
+++ standard-modules/crypto/crypto.js
@@ -1,20 +1,42 @@
 const crypto = require('crypto')
 
-const payload = {
-    user: 'gabriel.freitas',
-    password: '12345678'
-}
+// exibir formatos suportados de hash e cifras
+console.log('Supported hashes:', crypto.getHashes())
+console.log('Supported ciphers:', crypto.getCiphers())
 
-console.log(payload)
+// async random bytes
+crypto.randomBytes(16, (err, buf) => {
+    console.log(buf.toString('hex'))
+})
 
-payload.password = crypto
-    .createHash('sha1')
-    .update(payload.password)
+// sync random bytes
+let iv = crypto.randomBytes(16);
+console.log(iv.toString('hex'))
+
+// create hash
+let hash = crypto
+    .createHash('sha256')
+    .update('Minha mensagem')
     .digest('hex')
+console.log(hash)
 
-console.log(payload)
+// aes 256-bit cipher block chaining (cbc) encryption/decryption
+let secretMessage = 'Alou, me ouve?'
+let key = '12345678123456781234567812345678'
 
-crypto.DEFAULT_ENCODING = 'hex'
+let cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
+let encrypted = cipher.update(secretMessage, 'utf-8', 'hex')
+encrypted += cipher.final('hex')
+
+console.log('encrypted:', encrypted)
+
+let decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
+let decrypted = decipher.update(encrypted, 'hex', 'utf-8');
+decrypted += decipher.final('utf-8')
+
+console.log('decrypted:', decrypted)
+
+// crypto.DEFAULT_ENCODING = 'hex'
 
 console.time('syncTimeline')
 crypto.pbkdf2Sync('secret', 'salt', 10000, 512, 'sha512')
