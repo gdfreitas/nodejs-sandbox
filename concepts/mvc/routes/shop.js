b65656fec883625f30937de82f959e060f1d6f0b
Gabriel De Freitas <gabrieldfreitas9@gmail.com>
2018-12-01 17:55:49 -0200

    novas rotas

--- concepts/mvc/routes/shop.js
+++ concepts/mvc/routes/shop.js
@@ -7,6 +7,7 @@ const router = express.Router();
 router.get('/', shopController.getIndex);
 router.get('/products', shopController.getProducts);
 router.get('/cart', shopController.getCart);
+router.get('/orders', shopController.getOrders);
 router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
