455e66c5bb632cec51d863de21a1940cdf156d74
Gabriel De Freitas <gabrieldfreitas9@gmail.com>
2018-12-01 17:38:36 -0200

    registrando rotas

--- concepts/mvc/controllers/admin.js
+++ concepts/mvc/controllers/admin.js
@@ -18,13 +18,10 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
     Product.fetchAll((products) => {
-        res.render('shop/product-list', {
+        res.render('admin/products', {
             prods: products,
-            pageTitle: 'Shop',
-            path: '/',
-            hasProducts: products.length > 0,
-            activeShop: true,
-            productCSS: true
+            pageTitle: 'Admin Products',
+            path: '/admin/products',
         });
     });
-};
+}
