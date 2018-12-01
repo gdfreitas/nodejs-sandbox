d1e9d70fbd3bc4c100d36534e0a343cfb4f68bd1
Gabriel De Freitas <gabrieldfreitas9@gmail.com>
2018-12-01 16:53:30 -0200

    separando model

--- concepts/mvc/controllers/products.js
+++ concepts/mvc/controllers/products.js
@@ -1,27 +1,29 @@
-const products = [];
+const Product = require('../models/product')
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
-  });
+    res.render('add-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        formsCSS: true,
+        productCSS: true,
+        activeAddProduct: true
+    });
 };
 
 exports.postAddProduct = (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect('/');
+    const product = new Product(req.body.title);
+    product.save()
+    res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true
-  });
+    const products = Product.fetchAll();
+    res.render('shop', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/',
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true
+    });
 };
