535cbe7c85749e165a0fe73ce6b972aa3eba0f53
Gabriel De Freitas <gabrieldfreitas9@gmail.com>
2018-12-19 21:17:56 -0200

    traceroute

---
+++ nodejs/community-modules/traceroute/traceroute.js
@@ -0,0 +1,23 @@
+const Traceroute = require('nodejs-traceroute');
+
+try {
+    const tracer = new Traceroute();
+
+    tracer
+        .on('pid', (pid) => {
+            console.log(`pid: ${pid}`);
+        })
+        .on('destination', (destination) => {
+            console.log(`destination: ${destination}`);
+        })
+        .on('hop', (hop) => {
+            console.log(`hop: ${JSON.stringify(hop)}`);
+        })
+        .on('close', (code) => {
+            console.log(`close: code ${code}`);
+        });
+
+    tracer.trace('github.com');
+} catch (ex) {
+    console.log(ex);
+}
