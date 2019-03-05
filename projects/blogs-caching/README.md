669a91d4b759b381cee31943fb576f03b8dcc528
Gabriel De Freitas <gabrieldfreitas9@gmail.com>
2019-03-05 19:27:37 -0300

    cópia projeto inicial advanced nodejs course @ udemy;

---
+++ projects/blogs-caching/README.md
@@ -0,0 +1,7 @@
+# Blogs w/ caching
+
+Trying out some strategies of caching with a blogster project.
+
+## References
+
+[Node JS: Advanced Concepts](https://www.udemy.com/advanced-node-for-developers)
---
+++ projects/blogs-caching/config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+    googleClientID:
+        '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+    googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+    mongoURI: 'mongodb+srv://blogs_caching:6Vmi2GcqFzYf4vue@mongodb-cluster-3jgux.mongodb.net/blogs_caching?retryWrites=true',
+    cookieKey: '123123123'
+};
