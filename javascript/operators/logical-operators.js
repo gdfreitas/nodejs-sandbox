57606e50f16926403d1dfbdd86de607fa4a382f0
Gabriel De Freitas <gabrieldfreitas9@gmail.com>
2019-05-30 21:01:19 -0300

    Adiciona alguns exemplos de operadores;

---
+++ javascript/operators/logical-operators.js
@@ -0,0 +1,17 @@
+// "!" negação
+console.log(!true) // false
+console.log(!false) // true
+
+// "%%" conjunção
+console.log(true && true) // true
+console.log(true && false) // false
+console.log(false && true) // false
+console.log(false && false) // false
+
+// "||" disjunção
+console.log(true || true) // true
+console.log(true || false) // false
+console.log(false || true) // false
+console.log(false || false) // false
+
+// Lembrando da regra do curto-circuito que encerra a expressão o mais rápido possível
