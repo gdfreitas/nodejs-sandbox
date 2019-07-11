57c84e839610eb00ccd6484a2bfb27ac1d7b789c
Gabriel De Freitas <gabrieldfreitas9@gmail.com>
2019-07-11 15:11:03 -0300

    Added puppeteer code example;

---
+++ community-modules/puppeteer/index.js
@@ -0,0 +1,10 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto('https://example.com');
+  await page.screenshot({ path: 'example.png' });
+
+  await browser.close();
+})();
