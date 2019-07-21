c5e25f9c0f23c1be17a19bb313428256673744eb
Gabriel De Freitas <gabrieldfreitas9@gmail.com>
2019-07-21 18:31:13 -0300

    Adds "npm_lifecycle_event" to example;

--- concepts/npm-scripts/app.js
+++ concepts/npm-scripts/app.js
@@ -3,7 +3,10 @@ const http = require('http')
 const SERVER_PORT = process.env.SERVER_PORT || 80;
 
 const server = http.createServer((req, res) => {
-    res.write('Hello, world!')
+  res.write('Hello, world!')
 })
 
-server.listen(SERVER_PORT, () => console.log(`Server listening on http://localhost:${SERVER_PORT}`))
+server.listen(SERVER_PORT, () => {
+  console.log(`> Fired by ${process.env.npm_lifecycle_event}`)
+  console.log(`Server listening on http://localhost:${SERVER_PORT}`)
+})
