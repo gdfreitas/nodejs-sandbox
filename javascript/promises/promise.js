be76d31fa0a1b49448d53b231e0a74af8fe277d6
Gabriel De Freitas <gabrieldfreitas9@gmail.com>
2019-08-31 18:25:03 -0300

    adiciona exemplo de catch de promise no then, como segundo argumento; aprimorado exemplos;

--- javascript/promises/promise.js
+++ javascript/promises/promise.js
@@ -1,28 +1,41 @@
-const promise =
-  new Promise((resolve, reject) => {
-    // resolve('resolvido');
-    reject('rejeitado')
-  })
-    .then(value => {
-      console.log(value);
-    })
-    .catch(err => {
-      console.log(err);
-    });
+const promise = new Promise((resolve, reject) => {
+  resolve({ message: 'Promise resolvida com sucesso' })
+});
 
-const promise2 = Promise.reject('rejeitado') //  Promise.resolve('resolvido')
-  .then(value => {
-    console.log(value);
-    return value + 1;
+// Sintaxe mais simples
+promise
+  .then(function (data) {
+    console.log(data);
   })
-  .then(value => {
-    // throw 'erro!'
-    console.log(value);
-    return value + 2;
-  })
-  .then(value => {
-    console.log(value);
-  })
-  .catch(err => {
-    console.log(err);
+  .catch(function (error) {
+    console.error(error);
   });
+
+const promiseWillNotFulfill = new Promise((resolve, reject) => {
+  reject({ code: 500, message: 'Oops, um erro ocorreu!' })
+});
+
+// Sintaxe diferente, passando o o callback do catch como segundo argumento do then
+promiseWillNotFulfill
+  .then(function (data) {
+    console.log(data);
+  }, function (error) {
+    console.error(error);
+  });
+
+// O catch ao final irá capturar erros qualquer um dos callbacks acima dele
+const promiseWithErrorThrownInTheMiddle = Promise.resolve({ username: 'gdfreitas' })
+  .then(function (data) {
+    console.log(`O identificador do usuário é ${data.username}`);
+  })
+  .then(function (data) {
+    // Simula erro intermediário
+    throw new Error('Este callback intermediário não pôde fazer seu trabalho')
+  })
+  .then(function (data) {
+    console.log(`Este console.log nunca será executado`, data);
+  })
+  .catch(function (err) {
+    console.error(err);
+  });
+
